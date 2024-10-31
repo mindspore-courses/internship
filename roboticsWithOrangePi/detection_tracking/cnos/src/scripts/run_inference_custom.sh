0cd2c01560dfeeda3a4037e99bb1da93c5b2e7db
yaxun <yyxunn@outlook.com>
2024-10-31 17:27:43 +0800

    detele some directories

--- roboticsWithOrangePi/detection_tracking/cnos/src/scripts/run_inference_custom.sh
+++
@@ -1,5 +0,0 @@
-export CAD_PATH=/home/yaxun/FoundationPose/demo_data/realDex/mesh/air_duster.obj
-export OUTPUT_DIR=/home/yaxun/FoundationPose/cnos/realDex_template
-export RGB_PATH=/home/yaxun/FoundationPose/demo_data/realDex/00temp/0.jpg
-# export RGB_PATH=/home/yaxun/FoundationPose/demo_data/realDex/rgb/73.jpg
-python -m src.scripts.inference_custom --template_dir $OUTPUT_DIR --rgb_path $RGB_PATH --stability_score_thresh 0.5
